9cf5b79f015c9419b43c66f09c7a85e2c20803b8
harada <e215631souta0828@gmail.com>
2026-02-01 19:10:20 +0900

    reset

--- js/about.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  document.querySelectorAll(".skill-level").forEach(bar => {
-    const width = bar.style.width;
-    bar.style.width = "0";
-    setTimeout(() => {
-      bar.style.width = width;
-    }, 300);
-  });
-});
